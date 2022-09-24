0aeec1106202c6821c1e36b573ef5a597d777a7f
Osama-Daja <54779323+Osama-Daja@users.noreply.github.com>
2022-09-24 18:37:38 +0300

    Initial Create

---
+++ API_Project/API_Project/Controllers/TestAPIController.cs
@@ -0,0 +1,40 @@
+﻿using API_Project.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestAPIController : Controller
+    {
+        static public List<string> users = new List<string>();
+
+        [HttpGet]
+        public object getUsers()
+        {
+            return Ok(users);
+        }
+
+        [HttpPost]
+        [Route("{user}")]
+        public object AddUser(string user)
+        {
+            users.Add(user);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{user}")]
+        public object DeleteUser(string user)
+        {
+            users.Remove(user);
+            return Ok();
+        }
+
+    }
+}
